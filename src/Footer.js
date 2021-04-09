7fcd8463c0026eae1d0701caf053f5da7d6d629c
Lucia-Madero <78430885+Lucia-Madero@users.noreply.github.com>
2021-04-09 17:11:26 +0200

    Added link to GitHub repository

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <small>
+        <a
+          href="https://github.com/Lucia-Madero/react-weather-app"
+          target="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Lucía Madero Aguado
+      </small>
+    </div>
+  );
+}
